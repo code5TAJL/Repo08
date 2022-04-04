d777817d76f94d15fa134267cb3d7ee789a19be3
code5TAJL <asmir.sulejmanovic@outlook.com>
2022-04-04 21:02:04 +0200

    Added ex09/Day09 exc..

---
+++ ex09/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+function randomInteger() {
+    return Math.floor(Math.random(0, 29) * 30);
+}
+randomInteger();
+
+console.log(randomInteger());
+// Only change code about this line
+module.exports = randomInteger;
